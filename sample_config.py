bfd90625d780917b38c936445a5fdbbbe2613ae0
adeswal47 <139943093+adeswal47@users.noreply.github.com>
2023-07-19 18:28:07 +0530

    sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -29,7 +29,7 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 40960
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 360
+    PROCESS_MAX_TIMEOUT = 3600
     # watermark file
     DEF_WATER_MARK_FILE = "AD"
 
