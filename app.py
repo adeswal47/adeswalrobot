09221f4dfba41c78f674f09f08e3fe0aad612cce
adeswal47 <139943093+adeswal47@users.noreply.github.com>
2023-07-19 17:08:39 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-#Thank you LazyDeveloper for helping developers in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '@LazyDeveloper'
+    return '@ankittdeswal'
 
 
 if __name__ == "__main__":
